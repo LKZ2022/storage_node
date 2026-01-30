4cf2639bcc2b785c700ec1818d813e1b6d031e5d
LKZ2022 <3251626446@qq.com>
2026-01-30 12:53:55 +0800

    Completed the encapsulation of the SockFD class

--- src/net/include/SocketHandle.hpp
+++ src/net/include/SocketHandle.hpp
@@ -2,14 +2,105 @@
 //Licensed under the Apache License, Version 2.0.
 
 #pragma once
-#include "SocketFD.hpp"
+#include <utility>
+#include <system_error>
 
+#ifdef _WIN32
+
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <mswsock.h>
+#include <io.h>
+#include <windows.h>
+#pragma comment(lib, "Mswsock.lib")
+
+#elif __linux__
+
+#include <sys/socket.h>
+#include <sys/sendfile.h>
+#include <unistd.h>
+#include <cerrno>
+
+#endif
 
 namespace ref_storage::net {
 
+    /* Here, we define a C class and use conditional compilation to encapsulate SOCKET (uint_ptr) on Windows systems
+     * and int (file descriptor) on Linux systems, along with their corresponding invalid values.
+     * At the same time, we encapsulate functions that are frequently used and differ between Windows and Linux systems.
+     * This approach can reduce the use of conditional compilation and improve portability. */
+
     class SocketHandle {
     private:
+#ifdef _WIN32
+        using NativeSocketType = SOCKET;
+        static constexpr NativeSocketType kInvalid = INVALID_SOCKET;
+#elif __linux__
+        using NativeSocketType = int;
+        static constexpr NativeSocketType kInvalid = -1;
+#endif
 
+        NativeSocketType _handle = kInvalid;
+
+    public:
+
+        explicit SocketHandle() noexcept;
+        // Constructed from a native handle, requires taking ownership.
+        explicit SocketHandle(NativeSocketType fd) noexcept;
+
+        // Move Constructor and Move Assignment Operator.
+        SocketHandle(SocketHandle&& other) noexcept : _handle(std::exchange(other._handle, kInvalid)) {}
+        SocketHandle& operator=(SocketHandle&& other) noexcept;
+
+        // Copying is prohibited to prevent security issues.
+        SocketHandle(const SocketHandle&) = delete;
+        SocketHandle& operator=(const SocketHandle&) = delete;
+
+        // Destructor.
+        ~SocketHandle() noexcept;
+
+        /* Because implicit conversion can lead to some uncontrollable risks,
+         * we completely prohibit implicit conversion here.
+         * Implicit conversions can lead to certain uncontrollable risks, so we completely prohibit implicit conversions here.
+         * For instance, in all the constructors mentioned above, we have used 'explicit'. */
+
+        /* Obtain the native value, similar to pointer dereferencing.
+         * This is the only permitted way to access a native handle. */
+        [[nodiscard]] NativeSocketType native_handle() const noexcept;
+
+        // We demonstrate the removal of all types of implicit conversion operations.
+        template<typename T>
+        operator T() const = delete;
+
+        // Allow displaying bool conversion for conditional evaluation.
+        explicit operator bool() const noexcept;
+
+        // Validity Check.
+        [[nodiscard]] bool is_valid_handle() const noexcept;
+
+        // Allow comparisons within the same type.
+        bool operator==(const SocketHandle& other) const noexcept;
+        bool operator!=(const SocketHandle& other) const noexcept;
+
+        // Remove comparisons with primitive types.
+        bool operator==(NativeSocketType) const = delete;
+        bool operator!=(NativeSocketType) const = delete;
+
+        // Resource Management.
+        NativeSocketType release_handle() noexcept;
+        void reset_handle(NativeSocketType new_handle = kInvalid) noexcept;
+        void close_handle() noexcept;
+
+        // The following encapsulates all native system functions (Windows and Linux).
+        int bind_handle(const struct sockaddr* addr, socklen_t addrlen) const;
+        [[nodiscard]] int listen_handle(int backlog = 10) const;
+        SocketHandle accept_handle(struct sockaddr* addr = nullptr, socklen_t* addrlen = nullptr) const;
+
+        // The following is the factory method.
+        static SocketHandle create_socket_handle();
+
+    private:
+        static void throw_last_error(const char* operation);
     };
 
 }
--- src/net/src/SocketHandle.cpp
+++ src/net/src/SocketHandle.cpp
@@ -4,8 +4,131 @@
 
 #include "../include/SocketHandle.hpp"
 
+#include <iostream>
+
+
 namespace ref_storage::net {
 
+    SocketHandle::SocketHandle() noexcept = default;
 
+    SocketHandle::SocketHandle(NativeSocketType fd) noexcept : _handle(fd) {}
 
+    SocketHandle & SocketHandle::operator=(SocketHandle &&other) noexcept {
+        if (this != &other) {
+            close_handle();
+            _handle = std::exchange(other._handle, kInvalid);
+        }
+        return *this;
+    }
+
+    SocketHandle::~SocketHandle() noexcept {
+        close_handle();
+    }
+
+    SocketHandle::NativeSocketType SocketHandle::native_handle() const noexcept {
+        return _handle;
+    }
+
+    SocketHandle::operator bool() const noexcept {
+        return _handle != kInvalid;
+    }
+
+    bool SocketHandle::is_valid_handle() const noexcept {
+        return static_cast<bool>(*this);
+    }
+
+    bool SocketHandle::operator==(const SocketHandle &other) const noexcept {
+        return _handle == other._handle;
+    }
+
+    bool SocketHandle::operator!=(const SocketHandle &other) const noexcept {
+        return !(*this == other);
+    }
+
+    SocketHandle::NativeSocketType SocketHandle::release_handle() noexcept {
+        return std::exchange(_handle, kInvalid);
+    }
+
+    void SocketHandle::reset_handle(NativeSocketType new_handle) noexcept {
+        close_handle();
+        _handle = new_handle;
+    }
+
+    void SocketHandle::close_handle() noexcept {
+        if (is_valid_handle()) {
+            // Set the socket's SO_LINGER option to handle data that hasn't been fully sent.
+            struct linger linger_opt{};
+            linger_opt.l_onoff = 1;
+            linger_opt.l_linger = 1;
+            setsockopt(_handle, SOL_SOCKET, SO_LINGER,
+                        reinterpret_cast<const char *>(&linger_opt), sizeof(linger_opt));
+
+            /* Stop sending and receiving data
+             * Set to non-blocking mode to avoid blocking. */
+#ifdef _WIN32
+            shutdown(_handle, SD_BOTH);
+
+            u_long mode = 1;
+            ioctlsocket(_handle, FIONBIO, &mode);
+#elif __linux__
+            shutdown(_handle, SHUT_RDWR);
+
+            int flags = fcntl(_handle, F_GETFL, 0);
+            fcntl(_handle, F_SETFL, flags | O_NONBLOCK);
+#endif
+
+            // Try turning it off.
+            int result;
+
+#ifdef _WIN32
+            result = closesocket(_handle);
+#elif __linux__
+            result = close(_handle);
+#endif
+
+            if (result == -1) {
+                std::cerr << "Warning: close_handle() failed: " << _handle << std::endl;
+            }
+
+            _handle = kInvalid;
+        }
+    }
+
+    int SocketHandle::bind_handle(const struct sockaddr *addr, socklen_t addrlen) const {
+        if (!is_valid_handle()) {
+            throw_last_error("bind_handle() failed: Invalid socket ");
+        }
+        return bind(_handle, addr, addrlen);
+    }
+
+    int SocketHandle::listen_handle(int backlog) const {
+        if (!is_valid_handle()) {
+            throw_last_error("listen_handle() failed: Invalid socket ");
+        }
+        return listen(_handle, backlog);
+    }
+
+    SocketHandle SocketHandle::accept_handle(struct sockaddr *addr, socklen_t *addrlen) const {
+        if (!is_valid_handle()) {
+            return SocketHandle(kInvalid);
+        }
+        NativeSocketType new_handle = accept(_handle, addr, addrlen);
+        return SocketHandle(new_handle);
+    }
+
+    SocketHandle SocketHandle::create_socket_handle() {
+        NativeSocketType sock = socket(AF_UNSPEC, SOCK_STREAM, 0);
+        if (sock == kInvalid) {
+            throw_last_error("socket() failed");
+        }
+        return SocketHandle(sock);
+    }
+
+    void SocketHandle::throw_last_error(const char *operation) {
+#ifdef _WIN32
+        throw std::system_error(WSAGetLastError(), std::system_category(), operation);
+#elif __linux__
+        throw std::system_error(errno, std::system_category(), operation);
+#endif
+    }
 }
